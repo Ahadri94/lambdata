9dec15a26754ab15b3cd3ef49d4e51c0a725ae0d
Ahadri94 <ahmed-hadri_94@hotmail.com>
2020-09-29 12:06:00 -0700

    Finally made pipy work

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name="lambdata-19", # the name that you will install via pip
     version="3.8",
     author="Ahmed Hadri",
-    author_email="hadri-ahmed@lambdastudents.com",
+    author_email="ahmed-hadri_94@hotmail.com",
     description="A short description",
     long_description=long_description,
     long_description_content_type="text/markdown", # required if using a md file for long desc
